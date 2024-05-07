19e0d14638c2eec066932bec0e0c4bacba9fcbd7
mesayusriana12 <mesayusriana12@gmail.com>
2024-05-07 16:15:58 +0700

    fix: update dummy user password hashing

--- src/seeders/20240507080344-dummy-user.js
+++ src/seeders/20240507080344-dummy-user.js
@@ -1,16 +1,20 @@
 'use strict';
 
+const bcrypt = require('bcryptjs')
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', null, {})
+
+    const hashedPassword = await bcrypt.hash('admin', 10)
 
     const users = [
       {
         id: 1,
         email: 'admin@local',
         username: 'admin',
-        password: 'admin',
+        password: hashedPassword,
         createdAt: new Date(),
         updatedAt: new Date()
       }
